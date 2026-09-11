dddfa1b158d7a200a44b10851ea33844501b0782
cuking <cuking998244353@gmail.com>
2026-09-11 22:33:38 +0800

    补齐比较指令的返回类型、指令集划分与六个谓词
    - 两个 helper 之前漏写返回类型，开 __AVX512BW__ 时无法编译，补为 __mmask32/__mmask8
    - epu64 实际只需 avx512f，移出 BW 块；epu16 仍留在 BW
    - 补上 EQ/LT/LE/NE/NLE/NLT 六个谓词，即 == != < <= > >=

--- simd/instruction/compare/cmp.cpp
+++ simd/instruction/compare/cmp.cpp
@@ -1,24 +1,54 @@
 #pragma once
 
+//等于 _MM_CMPINT_EQ
+//小于 _MM_CMPINT_LT
+//小于等于 _MM_CMPINT_LE
+//不等于 _MM_CMPINT_NE
+//大于 _MM_CMPINT_NLE
+//大于等于 _MM_CMPINT_NLT
+
+#ifdef __AVX512F__
+
+namespace ins_details
+{
+
+	SIMD_OPT __mmask8 mm512_cmpeq_epu64(__m512i a,__m512i b){return _mm512_cmp_epu64_mask(a,b,_MM_CMPINT_EQ);}
+	SIMD_OPT __mmask8 mm512_cmplt_epu64(__m512i a,__m512i b){return _mm512_cmp_epu64_mask(a,b,_MM_CMPINT_LT);}
+	SIMD_OPT __mmask8 mm512_cmple_epu64(__m512i a,__m512i b){return _mm512_cmp_epu64_mask(a,b,_MM_CMPINT_LE);}
+	SIMD_OPT __mmask8 mm512_cmpne_epu64(__m512i a,__m512i b){return _mm512_cmp_epu64_mask(a,b,_MM_CMPINT_NE);}
+	SIMD_OPT __mmask8 mm512_cmpnle_epu64(__m512i a,__m512i b){return _mm512_cmp_epu64_mask(a,b,_MM_CMPINT_NLE);}
+	SIMD_OPT __mmask8 mm512_cmpnlt_epu64(__m512i a,__m512i b){return _mm512_cmp_epu64_mask(a,b,_MM_CMPINT_NLT);}
+
+}
+
+make_i_2(operator==,ins_details::mm512_cmpeq_epu64,K8,avx512f,1,VU64x8,VU64x8)
+make_i_2(operator!=,ins_details::mm512_cmpne_epu64,K8,avx512f,1,VU64x8,VU64x8)
+make_i_2(operator<,ins_details::mm512_cmplt_epu64,K8,avx512f,1,VU64x8,VU64x8)
+make_i_2(operator<=,ins_details::mm512_cmple_epu64,K8,avx512f,1,VU64x8,VU64x8)
+make_i_2(operator>,ins_details::mm512_cmpnle_epu64,K8,avx512f,1,VU64x8,VU64x8)
+make_i_2(operator>=,ins_details::mm512_cmpnlt_epu64,K8,avx512f,1,VU64x8,VU64x8)
+
+#endif
+
 #ifdef __AVX512BW__
 
 namespace ins_details
 {
 
-	//等于 _MM_CMPINT_EQ
-	//小于 _MM_CMPINT_LT
-	//小于等于 _MM_CMPINT_LE
-	SIMD_OPT mm512_cmple_epu16(__m512i a,__m512i b){return _mm512_cmp_epu16_mask(a,b,_MM_CMPINT_LE);}
-	SIMD_OPT mm512_cmple_epu64(__m512i a,__m512i b){return _mm512_cmp_epu64_mask(a,b,_MM_CMPINT_LE);}
-	//不等于 _MM_CMPINT_NE
-	//大于 _MM_CMPINT_NLE
-	//大于等于 _MM_CMPINT_NLT
+	SIMD_OPT __mmask32 mm512_cmpeq_epu16(__m512i a,__m512i b){return _mm512_cmp_epu16_mask(a,b,_MM_CMPINT_EQ);}
+	SIMD_OPT __mmask32 mm512_cmplt_epu16(__m512i a,__m512i b){return _mm512_cmp_epu16_mask(a,b,_MM_CMPINT_LT);}
+	SIMD_OPT __mmask32 mm512_cmple_epu16(__m512i a,__m512i b){return _mm512_cmp_epu16_mask(a,b,_MM_CMPINT_LE);}
+	SIMD_OPT __mmask32 mm512_cmpne_epu16(__m512i a,__m512i b){return _mm512_cmp_epu16_mask(a,b,_MM_CMPINT_NE);}
+	SIMD_OPT __mmask32 mm512_cmpnle_epu16(__m512i a,__m512i b){return _mm512_cmp_epu16_mask(a,b,_MM_CMPINT_NLE);}
+	SIMD_OPT __mmask32 mm512_cmpnlt_epu16(__m512i a,__m512i b){return _mm512_cmp_epu16_mask(a,b,_MM_CMPINT_NLT);}
 
+}
 
-
-};
-
+make_i_2(operator==,ins_details::mm512_cmpeq_epu16,K32,avx512bw,1,VU16x32,VU16x32)
+make_i_2(operator!=,ins_details::mm512_cmpne_epu16,K32,avx512bw,1,VU16x32,VU16x32)
+make_i_2(operator<,ins_details::mm512_cmplt_epu16,K32,avx512bw,1,VU16x32,VU16x32)
 make_i_2(operator<=,ins_details::mm512_cmple_epu16,K32,avx512bw,1,VU16x32,VU16x32)
-make_i_2(operator<=,ins_details::mm512_cmple_epu64,K8,avx512bw,1,VU64x8,VU64x8)
+make_i_2(operator>,ins_details::mm512_cmpnle_epu16,K32,avx512bw,1,VU16x32,VU16x32)
+make_i_2(operator>=,ins_details::mm512_cmpnlt_epu16,K32,avx512bw,1,VU16x32,VU16x32)
 
 #endif
